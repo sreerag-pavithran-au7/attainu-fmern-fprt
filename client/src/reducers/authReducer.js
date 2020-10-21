e4a301c77d8a48b994ab838baf6015dad48b083e
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-10-21 21:19:56 +0530

    Create authReducer.js

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,21 @@
+import {SET_CURRENT_USER} from "../types";
+import {isEmpty} from "../utils/validation/isEmpty";
+
+const initialState={
+    isAuthenticated:false,
+    user:{}
+};
+
+export const authReducer= (state=initialState, action)=>{
+  switch (action.type) {
+      case SET_CURRENT_USER: 
+          return {
+              ...state,
+              isAuthenticated: !isEmpty(action.payload),
+              user:action.payload
+          }
+      default: return state;
+  }
+};
+
+export default authReducer;
